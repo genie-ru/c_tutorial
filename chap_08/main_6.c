b1e4e0a747c53d383d88d28b7180ce04e00216b3
genie-ru <genie.zgmf987@gmail.com>
2023-10-30 19:39:40 +0900

    test

---
+++ chap_08/main_6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int no;
+    scanf("%d", &no);
+    if (no == 1) {
+        printf("野比のび太\n");
+    } else if {
+        printf("源静香\n");
+    } else if {
+        printf("剛田武\n");
+    } else if {
+        printf("骨川スネ夫\n");
+    } else if {
+        printf("そんな番号の人はいない\n");
+    }
+
+    return 0;
+}
